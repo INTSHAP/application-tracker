3fbb0c8f078a4494bcde29ee123d44f8fbfa67e5
Dammymure <dammymure1@gmail.com>
2024-04-29 01:14:29 +0100

    job UI

--- src/components/job/JobCard.tsx
+++ src/components/job/JobCard.tsx
@@ -1,59 +1,13 @@
-const dummyJobs = [
-  {
-    company: "Acme Corporation",
-    title: "Software Engineer",
-    location: "San Francisco, CA",
-    minExperience: 3,
-    pay: 100000,
-    jobId: "JOB123456",
-    nature: "Full-time",
-    opening_date: "2024-04-15",
-    closing_date: "2024-05-15",
-    skills_required: ["JavaScript", "React", "Node.js"],
-    applicants: [],
-  },
-  {
-    company: "Tech Innovations Ltd",
-    title: "Data Scientist",
-    location: "New York, NY",
-    minExperience: 5,
-    pay: 120000,
-    jobId: "JOB987654",
-    nature: "Remote",
-    opening_date: "2024-04-18",
-    closing_date: "2024-05-18",
-    skills_required: ["Python", "Machine Learning", "Data Analysis"],
-    applicants: [],
-  },
-  {
-    company: "Global Logistics Inc",
-    title: "Logistics Manager",
-    location: "London, UK",
-    minExperience: 7,
-    pay: 80000,
-    jobId: "JOB567890",
-    nature: "Contract",
-    opening_date: "2024-04-20",
-    closing_date: "2024-05-20",
-    skills_required: [
-      "Supply Chain Management",
-      "Inventory Control",
-      " Management",
-    ],
-    applicants: [],
-  },
-];
-
-const AdminLanding: React.FC = () => {
+const JobCard = ({ jobs }) => {
   return (
     <div className="">
       <div> Welcome (recreuitername)</div>
 
       <div className="job-list w-[85%] mx-auto">
-        {dummyJobs.map((job, index) => (
+        {jobs.map((job, index) => (
           <div
             key={index}
-            className="my-10 border-red-600 border-2 rounded shadow-lg px-5 hover:bg-slate-100 duration-150 ease-out md:ease-in cursor-pointer"
+            className="my-10 border-red-700 border- rounded shadow-lg px-5 hover:bg-slate-100 duration-150 ease-out md:ease-in cursor-pointer"
           >
             <div className="flex gap-3 justify-between py-2">
               <p className="text-sm font-semibold">{job.company}</p>
@@ -105,4 +59,4 @@ const AdminLanding: React.FC = () => {
   );
 };
 
-export default AdminLanding;
+export default JobCard;
